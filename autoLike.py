c3b089241e79c849299c5d64d7445e3897d36c2e
A-SK <eisuke_nakanishi@12knox.com>
2018-05-17 14:02:52 +0900

    Initial push.

---
+++ autoLike.py
@@ -0,0 +1,25 @@
+import tweepy
+import time
+import sys
+from datetime import datetime
+
+CONSUMER_KEY = 'PhmPFIDjTcbaxeRmqXqK0KZVR'
+CONSUMER_SECRET = 'TLIWUadER3B5zySncWwHT7aG1Vyg8evWelvfT9Z9wXXznqQ6uR'
+ACCESS_TOKEN = '87425177-4dkwezCSg5Ux58X2NId3WE2XilzzSX9K7hv2KG9KQ'
+ACCESS_SECRET = '6htnT5yE0zgGC67lfHZ4y9DU21jGwufRtDkfXgQZoJKW7'
+
+#Tweepy
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+#APIインスタンスを作成
+api = tweepy.API(auth)
+
+print('done!')
+
+args = sys.argv
+query = args[1]
+
+tweets =  searchTweets(query)
+likeTweets(tweets)
+
+def searchTweets:
